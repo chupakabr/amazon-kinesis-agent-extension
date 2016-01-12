c36a34d0a0710b633ee46c125227cd06d31e49ea
Valeriy Chevtaev <myltik@gmail.com>
2016-01-12 13:58:55 +0000

    First commit

---
+++ src/main/java/com/amazon/kinesis/streaming/agent/extension/AgentExtensionExample.java
@@ -0,0 +1,9 @@
+package com.amazon.kinesis.streaming.agent.extension;
+
+/**
+ * Created by myltik on 12/01/2016.
+ */
+public class AgentExtensionExample implements IAgentExtension {
+    // TODO
+
+}
---
+++ src/main/java/com/amazon/kinesis/streaming/agent/extension/IAgentExtension.java
@@ -0,0 +1,8 @@
+package com.amazon.kinesis.streaming.agent.extension;
+
+/**
+ * Created by myltik on 12/01/2016.
+ */
+public interface IAgentExtension {
+    // TODO
+}
